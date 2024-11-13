b384c3e62cb03a6324e95c61bed11bd02b78bc76
jeancalistro <jeeanrafael456@gmail.com>
2024-11-13 17:35:17 -0300

    first commit

---
+++ src/main/java/com/jeancalistro/api_plataforma_curso/ApiPlataformaCursoApplication.java
@@ -0,0 +1,13 @@
+package com.jeancalistro.api_plataforma_curso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiPlataformaCursoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiPlataformaCursoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jeancalistro/api_plataforma_curso/ApiPlataformaCursoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jeancalistro.api_plataforma_curso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiPlataformaCursoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
